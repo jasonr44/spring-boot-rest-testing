c37b7990543d6f986199a3b1b8c05aefa19f4747
Jason Ransom <jasonlawrenceransom@gmail.com>
2020-11-30 19:14:07 -0500

    init

---
+++ src/main/java/com/example/springbootresttesting/SpringbootresttestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootresttesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootresttestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootresttestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootresttesting/SpringbootresttestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootresttesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootresttestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
